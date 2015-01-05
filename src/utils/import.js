6d17740819707f3a4e46f56cd28e03a2cfd7bb56
wayne-o <wayne@made-software.com>
2015-01-05 10:07:28 +0000

    trying to import using waterline ORM

--- src/utils/import.js
+++ src/utils/import.js
@@ -1,8 +1,77 @@
 console.log('importing g2013');
 
-var request = require("request")
+var request = require("request");
+var Waterline = require('waterline');
+var url = "http://clashfinder.com/data/event/g2013.json";
+var orm = new Waterline();
+var mongoAdapter = require('sails-mongo');
 
-var url = "http://clashfinder.com/data/event/g2013.json"
+var config = {
+
+  adapters: {
+    	'default': mongoAdapter,
+    	mongo: mongoAdapter
+  },
+
+  connections: {
+    myLocalMongo: {
+      	adapter: 'mongo',
+    	host: 'localhost',
+    	port: 27017,
+    }
+  },
+
+  defaults: {
+    migrate: 'alter'
+  }
+
+};
+
+var EventLocation = Waterline.Collection.extend({
+	identity: 'location',
+	connection: 'myLocalMongo',
+	attributes: {
+
+    	name : { type: 'string' },
+
+   		/*eventInstance : {
+        	model:'eventInstance'
+    	}*/
+  	}
+});
+
+var EventInstance = Waterline.Collection.extend({
+	identity: 'eventInstance',
+	connection: 'myLocalMongo',
+	attributes: {
+
+	    name : { type: 'string' },
+
+	    dateFrom : { type: 'date' },
+
+	    dateTo : { type: 'date' },
+
+	   /* locations : {
+	        collection: 'location',
+	        via: 'eventInstance'
+	    }*/
+  	}
+});
+
+
+orm.loadCollection(EventLocation);
+orm.loadCollection(EventInstance);
+
+
+orm.initialize(config, function(err, models) {
+  if(err) throw err;
+
+  app.models = models.collections;
+  app.connections = models.connections;
+
+  // Start Server
+  app.listen(3000);
+});
 
 request({
     url: url,
@@ -11,29 +80,45 @@ request({
 
     if (!error && response.statusCode === 200) {
 
-
     	console.log(body.name);
-		request.post(
-		    'http://127.0.0.1:1337/api/v1/eventinstances',
-		   { EventInstance: { Name: body.name } },
-		    function (error, response, body) {
-		        if (!error && response.statusCode == 200) {
-		            console.log(body)
-		        }
-		    }
-		);
 
-        for (var i = body.locations.length - 1; i >= 0; i--) {
-        	var location = body.locations[i];
-        	request.post(
-		    'http://127.0.0.1:1337/api/v1/locations',
-		   { Location : { Name: location.name } },
-		    function (error, response, body) {
-		        if (!error && response.statusCode == 200) {
-		            console.log(body)
-		        }
+    	var locations = new Array(); 
+		
+		var ei = {
+		  eventinstance: {
+		  	name: body.name,
+		  }
+		};
+
+		console.log(ei);
+
+		app.models.eventInstance.create(ei, function(err, model) {
+		    if(err) {
+		    	console.log(err);
 		    }
-		);
-        };
+		   
+		   	var id = model.id;
+		   	console.log('And the ID is!!! ' + id)
+		});
+
+		/*
+
+				for (var i = body.locations.length - 1; i >= 0; i--) {
+					
+					var l = {
+						location: {
+							name : body.locations[i].name,
+							eventInstance : resultObject
+						}
+					};
+					
+		    		sendEventLocation('/api/v1/locations', l, function(locationResult){
+		    			console.log(locationResult);
+		    		});
+		    	}
+
+		*/
+
+		
     }
 })
