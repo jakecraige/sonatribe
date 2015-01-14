d16a77eda072909f9cdb112e7a360dad6bb38c54
wayne-o <wayne@made-software.com>
2015-01-14 13:02:14 +0000

    cleaning up auth - not using torii adapter any more

--- src/sonatribe-ui/app/routes/application.js
+++ src/sonatribe-ui/app/routes/application.js
@@ -2,6 +2,19 @@ import Ember from 'ember';
 import SonatribeRoute from 'sonatribe-ui/routes/sonatribe';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
+function fblogin() {
+	return new Ember.RSVP.Promise(function(resolve, reject){
+		FB.login(function(response){
+			if (response.authResponse) {
+				Ember.run(null, resolve, response.authResponse);
+			} else {
+				Ember.run(null, reject, response.status);
+			}
+		}, {scope: 'email'});
+	});
+}
+
+
 var ApplicationRoute = SonatribeRoute.extend(ApplicationRouteMixin, {
 	 actions: {
 			// action to trigger authentication with Torii
@@ -10,6 +23,34 @@ var ApplicationRoute = SonatribeRoute.extend(ApplicationRouteMixin, {
 				var rte = this;
 
 				this.get('session')
+					.authenticate('simple-auth-authenticator:torii', 'facebook-oauth2')
+					.then(function(){
+
+						console.log('authenticated')
+
+						console.log(rte.get('session'));
+
+						var _this = this;
+
+						//this is fugging nasty as we have to fire a login twice - once for torii
+						// and again for us to get the access_token
+						// definitely need to revisit this
+						fblogin().then(function(response){
+
+							Ember.$.ajax({
+								url: 'http://dev.festivaltribe.co.uk:1337/auths/facebook_access_token?code=' + response.accessToken,
+								dataType: 'json',
+								success: function(authResponse){
+									console.log(authResponse);
+								},
+								error: function(err){
+									console.log(err);
+								}
+							});
+						});
+					});
+
+				/*this.get('session')
 					.authenticate('authenticator:torii-st',
 					{
 						torii:    this.get('torii'),
@@ -20,7 +61,7 @@ var ApplicationRoute = SonatribeRoute.extend(ApplicationRouteMixin, {
 						console.log('authenticated')
 
 						console.log(rte.get('session'));
-					});
+					});*/
 			},
 
 	 		showLogin: function() {
