61a53ca62cd3811e035ce2828dffb5bc69e50532
wayne-o <wayne@prjatk.com>
2015-01-14 15:53:09 +0000

    Create entrypoint.sh

---
+++ setup/docker/api/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+sudo apt-get -y update
+apt-get install -y ruby ruby-dev
+gem install compass
+apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | tee -a /etc/apt/sources.list.d/10gen.list
+apt-get -y update
+apt-get -y install mongodb-10gen
+apt-get install -y python-software-properties python g++ make git
